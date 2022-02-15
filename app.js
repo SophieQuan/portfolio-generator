c1e1dcead300873dcd65922fb21fd1d01a75bf0c
SophieQuan <sophiequan7@gmail.com>
2022-02-15 18:58:13 -0500

    install inquirer and prompt user name

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then (answers => console.log(answers));
+// const fs = require('fs');
 
-const generatePage = require('./src/page-templete');
+// const generatePage = require('./src/page-templete');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github)
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML , err => {
+//     if (err) throw err;
 
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Porfolio complete! Check out index.html to see the output! ')
-});
+//     console.log('Porfolio complete! Check out index.html to see the output! ')
+// });
